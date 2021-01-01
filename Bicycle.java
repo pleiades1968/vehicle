163efdfbe3292457d63eed3bbeb1323ec3bec091
Kenichi Ohtsuka <pleiades.11.1968@gmail.com>
2021-01-01 21:15:09 +0900

    first commit

---
+++ Bicycle.java
@@ -0,0 +1,24 @@
+class Bicycle {
+  private String name;
+  private String color;
+  private int distance = 0;
+  
+  // インスタンスの生成時にフィールドに値をセットできるよう、コンストラクタを用意
+  Bicycle(String name, String color) {
+    this.name = name;
+    this.color = color;
+  }
+  
+  // Mainクラスからnameフィールドの値を取得するためにゲッターを定義
+  public void printData() {
+    System.out.println("名前：" + this.name);
+    System.out.println("色：" + this.color);
+    System.out.println("走行距離：" + this.distance + "km");
+  }
+  
+  public void run(int distance) {
+    System.out.println(distance + "km走ります");
+    this.distance += distance;
+    System.out.println("走行距離：" + this.distance + "km");
+  }  
+}
